d3dcd3fea428f4f75c936ff1bb6f658730010c69
Choirul Andriansyah <choirulandriansyah@choiruls-MacBook-Air.local>
2021-07-15 05:11:09 +0700

    add endpoint

--- src/main/java/com/choirula/learninggit/controller/LearningGitController.java
+++ src/main/java/com/choirula/learninggit/controller/LearningGitController.java
@@ -1,8 +1,14 @@
 package com.choirula.learninggit.controller;
 
+import com.choirula.learninggit.service.LearningGitService;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class LearningGitController {
 
-    public String test(){
+    @Autowired
+    private LearningGitService learningGitService;
+
+    public void test(){
 
     }
 }
